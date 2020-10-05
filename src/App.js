60387fbca5587b3fe1e67a7933bed09174adc152
puci2020 <adam.pucicki97@gmail.com>
2020-10-05 14:59:00 +0200

    Add auth with firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, {useState, useEffect} from 'react';
 import styled from "styled-components";
 import Layout from "./Theme/Layout";
 import Post from "./Post";
-import {db} from "./firebase";
+import {db, auth} from "./firebase";
 import {Modal, Button, Input} from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 
 
 const Wrapper = styled.div`
@@ -68,8 +68,8 @@ const Img = styled.img`
 `;
 
 function getModalStyle() {
-    const top = 50
-    const left = 50
+    const top = 50;
+    const left = 50;
 
     return {
         top: `${top}%`,
@@ -89,6 +89,9 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         flexDirection: 'column',
     },
+    item: {
+        margin: 5,
+    }
 }));
 
 function App() {
@@ -99,7 +102,31 @@ function App() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [user, setUser] = useState(null);
 
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                console.log(authUser);
+                setUser(authUser);
+
+                if (authUser.displayName){
+
+                }else{
+                    return authUser.updateProfile({
+                        displayName: username
+                    })
+                }
+            } else {
+                setUser(null);
+            }
+        });
+//1:55
+        return () => {
+            unsubscribe();
+        }
+
+    }, [user, username]);
 
     useEffect(() => {
         db.collection('posts').onSnapshot(snapshot => {
@@ -114,10 +141,13 @@ function App() {
         setOpen(true);
     };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+    const signUp = (e) => {
+        e.preventDefault();
 
+        auth.createUserWithEmailAndPassword(email, password)
+            .catch((error) => alert(error.message))
+
+    };
 
 
     return (
@@ -125,33 +155,41 @@ function App() {
             <Wrapper>
                 <Modal
                     open={open}
-                    onClose={handleClose}
+                    onClose={() => setOpen(false)}
                     aria-labelledby="simple-modal-title"
                     aria-describedby="simple-modal-description"
                 >
-                    <div style={modalStyle} className={classes.paper}>
-                        <Img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-                             alt="Instagram Logo"
+                    <form style={modalStyle} className={classes.paper}>
+
+                        <Img
+                            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                            alt="Instagram Logo"
                         />
                         <Input
                             placeholder="Username"
                             type="text"
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
+                            className={classes.item}
                         />
                         <Input
                             placeholder="Email"
                             type="text"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            />
+                            className={classes.item}
+                        />
                         <Input
                             placeholder="Password"
                             type="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
+                            className={classes.item}
                         />
-                    </div>
+                        <Button type="submit" onClick={signUp}
+                                className={classes.item}>Sign Up</Button>
+
+                    </form>
                 </Modal>
                 <Header>
                     <Img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
