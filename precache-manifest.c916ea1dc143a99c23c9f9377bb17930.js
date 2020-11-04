193c93767528c5f5a78c7ff9d1e16ca67a046f4c
puci2020 <adam.pucicki97@gmail.com>
2020-11-04 14:33:58 +0100

    Updates

---
+++ precache-manifest.c916ea1dc143a99c23c9f9377bb17930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6267ccbf94f82bb8d863333264eaea29",
+    "url": "/Instagram-React-Firebase/index.html"
+  },
+  {
+    "revision": "74d7f95c7a56550687c0",
+    "url": "/Instagram-React-Firebase/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e236e2d222e66f8e5777",
+    "url": "/Instagram-React-Firebase/static/js/2.91e8303a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Instagram-React-Firebase/static/js/2.91e8303a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d7f95c7a56550687c0",
+    "url": "/Instagram-React-Firebase/static/js/main.cd787381.chunk.js"
+  },
+  {
+    "revision": "95c934455e11f0c5bb2e",
+    "url": "/Instagram-React-Firebase/static/js/runtime-main.a16fc931.js"
+  },
+  {
+    "revision": "17d7183a7cb8f5a6b14bb6afed306e9a",
+    "url": "/Instagram-React-Firebase/static/media/2.17d7183a.jpg"
+  }
+]);
